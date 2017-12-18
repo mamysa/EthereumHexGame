602026b7c1e323eb58d0fd039b0ad3913de46556
mamysa <alexmamyshev@fastmail.com>
2017-12-18 15:41:18 +0100

    cleanup contract

--- src/contracts/GameFactory.sol
+++ src/contracts/GameFactory.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.0;
 contract GameFactory {
 
+	address owner;
+
 	struct GameInfo {
 		bool    isset;
 		address player1;
@@ -8,24 +10,23 @@ contract GameFactory {
 		address gameInstance;
 	}
 
-	address owner;
-	bytes32 version;
+
+	// mapping player -> gameinfo mapping.
 
 	// player who is waiting for someone else call startGame()
 	address awaitingPlayer;
 	
 	// currently active games
-	mapping(address => GameInfo) public liveGames;
+	mapping(address => GameInfo) public gameInstances;
 
-	
+	event onGameInstanceCreated(address gameInstance, address p1, address p2);
+	event onGameInstanceDestroyed(address gameInstance, address p1, address p2);
 
-	function GameFactory(bytes32 _version) public {
+	function GameFactory() public {
 		owner = msg.sender;
-		version = _version;
 		awaitingPlayer = address(0x0);
 	}
 
-	
 	// If address of awaitingPlayer is 0, store msg.sender there.
 	// Otherwise, initialize GameInfo structure, store GameInfo into the mapping, and
 	// initialize game instance. 
@@ -37,34 +38,38 @@ contract GameFactory {
 			awaitingPlayer = msg.sender;
 		}
 		else {
-			//TODO instantiate contract 
-			address p1 = awaitingPlayer;
+			address p1 = awaitingPlayer; 
 			address p2 = msg.sender;
 			HexGameInstance game = new HexGameInstance(this, p1, p2);
-			liveGames[p1] = GameInfo(true, p1, p2, game);
-			liveGames[p2] = GameInfo(true, p1, p2, game);
+			GameInfo memory info = GameInfo(true, p1, p2, game);
+			gameInstances[p1] = info; gameInstances[p2] = info; 
 			awaitingPlayer = address(0x0);
+			onGameInstanceCreated(game, p1, p2);
 		}
 	}
 
 	function endGame(address gameInstance, address p1, address p2) checkEndGameCaller(gameInstance, p1, p2) public {
-		delete liveGames[p1];
-		delete liveGames[p2];
+		onGameInstanceDestroyed(gameInstance, p1, p2);
+		delete gameInstances[p1];
+		delete gameInstances[p2];
 	}
 
-	/**
-	* Ensure that user is not already waiting / playing the game.
-	*/
+	/*
+	 * Ensure that user is not already waiting / playing the game.
+	 */
 	modifier checkUser() {
+		require(!gameInstances[msg.sender].isset);
 		require(msg.sender != awaitingPlayer);
-		require(!liveGames[msg.sender].isset);
 		_;
 	}
 
+	/*
+	 * Verify that game can only be ended by someone that we expect.
+	 */
 	modifier checkEndGameCaller(address gameInstance, address p1, address p2) {
 		require(msg.sender == gameInstance);
-		require(liveGames[p1].gameInstance == gameInstance);
-		require(liveGames[p2].gameInstance == gameInstance);
+		require(gameInstances[p1].gameInstance == gameInstance);
+		require(gameInstances[p2].gameInstance == gameInstance);
 		_;
 	}
 }
@@ -73,9 +78,7 @@ contract GameFactory {
 contract HexGameInstance {
 	
 	enum PieceColor { RED, BLU, NONE }
-
 	enum CellType { ALL, REDTARGET, BLUTARGET, ANYTARGET }
-
 
 	// Represents cell of the game field. 
 	// TODO CellType?
@@ -83,74 +86,107 @@ contract HexGameInstance {
 	//It can also be empty (i.e. PieceColor.NONE)
 	struct Cell {
 		PieceColor piece;
-		CellType cellType;
+		CellType target;
 	}
 
 	// parent contract that we will call once game is over.
 	GameFactory parentContract;
 
 	// mapping of player to piece color. Also store player addresses in separate array.
-	mapping(address => PieceColor) public mappingPlayerColor;
+	mapping(address => PieceColor) public playerColor;
 	address[2] players;
 
 	// index of the player who has the next turn. _player1 if 0, _player2 if 1 
 	uint8 turn;
 
 	// number of moves.
-	uint moves;
+	uint moveNumber;
+
+	uint constant BOARD_SIZE = 11;
 
 	// game board
-	Cell[11][11] public gameField;
+	Cell[BOARD_SIZE][BOARD_SIZE] public gameField;
 
-	event onGameStarted(address gameInstance, address p1, address p2);
+	event onGameStarted(address gameInstance, address p1, PieceColor p1color, address p2, PieceColor p2color, uint movenum);
 	event onGameEnded(address winner);
-	event onPiecePlaced(uint x, uint y, PieceColor pieceColor);
-	//event onPieceSwapped();
+	event onPiecePlaced(uint x, uint y, PieceColor pieceColor, address player, uint movenum);
+	event onPieceSwapped(uint movenum);
 	
 	function HexGameInstance(GameFactory _parentContract, address _player1, address _player2) public {
 		parentContract = _parentContract;
-		players[0] = _player1;
-		players[1] = _player2;
-		mappingPlayerColor[players[0]] = PieceColor.RED;
-		mappingPlayerColor[players[1]] = PieceColor.BLU;
-		onGameStarted(this, players[0], players[1]);
-	}
-
-	function placePiece(uint x, uint y, PieceColor color) checkValidMove(x, y, color) public {
-		gameField[y][x].piece = color;
-		// TODO check winning condition?
-		nextMove();
-		onPiecePlaced(x, y, color);
-	}
-
-	/*
-	function swapPiece() checkValidSwap() {
+		players[0] = _player1; playerColor[players[0]] = PieceColor.RED;
+		players[1] = _player2; playerColor[players[1]] = PieceColor.BLU;
 		
+		uint x;
+		uint y;
+		uint b = BOARD_SIZE - 1;
+
+		for (y = 0; y < BOARD_SIZE; y++) 
+		for (x = 0; x < BOARD_SIZE; x++)  {
+			gameField[y][x] = Cell(PieceColor.NONE, CellType.ALL);
+		}
+
+		for (x = 0; x < BOARD_SIZE; x++) {
+			gameField[0][x].target = CellType.REDTARGET;
+			gameField[b][x].target = CellType.REDTARGET;
+		}
+
+		for (y = 0; y < BOARD_SIZE; y++) {
+			gameField[y][0].target = CellType.BLUTARGET;
+			gameField[y][b].target = CellType.BLUTARGET;
+		}
+
+		// set corners of the board as target for any player
+		gameField[0][0].target = CellType.ANYTARGET;
+		gameField[0][b].target = CellType.ANYTARGET;
+		gameField[b][0].target = CellType.ANYTARGET;
+		gameField[b][b].target = CellType.ANYTARGET;
+
+		onGameStarted(this, players[0], playerColor[players[0]], 
+					        players[1], playerColor[players[1]]);
 	}
-	*/
+
+	function placePiece(uint x, uint y, PieceColor color) checkValidPiecePlacement(x, y, color) public {
+		gameField[y][x].piece = color;
+		onPiecePlaced(x, y, color, msg.sender, moveNumber);
+		nextMove();
+	}
+
+	function placePieceAndCheckWinningCondition(uint x, uint y, PieceColor color, uint[] positions) public {
+		placePiece(x, y, color);
+
+		// TODO check path.
+
+	}
+
+	function swapPieceColor() checkValidSwap() public {
+		playerColor[players[0]] = PieceColor.BLU;
+		playerColor[players[1]] = PieceColor.RED;
+		onPieceSwapped(moveNumber);
+		nextMove();
+	}
 
 	// kill contract / display win-lose messages to involved parties. 
-	// TODO in the future, we might make game entry payable?
 	function endGame() public {
 		onGameEnded(players[0]);
-		parentContract.endGame(this, players[0], players[1]);
+		//parentContract.endGame(this, players[0], players[1]);
 		selfdestruct(0x0);
 	}
 
 	// Increment move counter and give turn to the next player.
 	function nextMove() private {
-		moves += 1;
+		moveNumber += 1;
 		turn = (turn + 1) % 2;
 	}
 
 	// (1) Ensure that move is made by the person we expect;
 	// (2) Ensure that that person is not doing insertion outside the board.
 	// (3) Ensure that person is using a piece color that we expect.
-	modifier checkValidMove(uint x, uint y, PieceColor color) {
+	modifier checkValidPiecePlacement(uint x, uint y, PieceColor color) {
 		require(msg.sender == players[turn]);
-		require(x >= 0 && x < 11);
-		require(y >= 0 && y < 11);
-		require(mappingPlayerColor[msg.sender] == color);
+		require(x >= 0 && x < BOARD_SIZE);
+		require(y >= 0 && y < BOARD_SIZE);
+		require(playerColor[msg.sender] == color);
 		require(gameField[y][x].piece == PieceColor.NONE);
 		_;
 	}
@@ -159,7 +195,7 @@ contract HexGameInstance {
 	// (2) Ensure that swap move can be only made on the second move.
 	modifier checkValidSwap() {
 		require(msg.sender == players[turn]);
-		require(moves == 1);
+		require(moveNumber == 1);
 		_;
 	}
 }
